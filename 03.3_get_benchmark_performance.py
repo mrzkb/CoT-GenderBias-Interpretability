e806e46b126a5361b11fcb08c5339767f309b508
Edie <edieblue.p@icloud.com>
2025-11-06 12:36:57 -0500

    Changes to run on stereo and crows

---
+++ 03.3_get_benchmark_performance.py
@@ -0,0 +1,59 @@
+import pandas as pd
+from datetime import datetime
+import argparse
+import os
+
+# example_id | bias_type | context_condition | prompt | answer_texts | bias_label | unknown_label | predicted_answer
+
+def get_counts(df):
+    total = len(df)
+    n_unknown = (df['predicted_answer'] == df['unknown_label']).sum()
+    n_ster = (df['predicted_answer'] == df['bias_label']).sum()
+    n_antister = ((df['predicted_answer'] != df['unknown_label']) & (df['predicted_answer'] != df['bias_label'])).sum()
+
+    counts = {
+            "total": total,
+            "p_unknown": round((n_unknown / total) * 100, 2),
+            "p_ster": round((n_ster / total) * 100, 2),
+            "p_antister": round((n_antister / total) * 100, 2)
+            }
+
+    return counts
+
+def evaluate_responses(csv_name):
+   
+    # Load model responses
+    df = pd.read_csv(csv_name)
+
+    # Get accuracy, other counts...
+    performance = get_counts(df)
+
+    return performance
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Process prompts with LLM and compute log-likelihoods')
+    parser.add_argument('--model_name', type=str, required=True,
+                        help='Short name for the model (used in output filenames)') 
+    args = parser.parse_args()
+ 
+    NCOT = evaluate_responses(f'data/stereo/{args.model_name}_responses_gender_no_cot.csv')
+    COT = evaluate_responses(f'data/stereo/{args.model_name}_responses_gender_cot.csv')
+
+    # NCOT = evaluate_responses(f'data/crows/{args.model_name}_responses_gender_no_cot.csv')
+    # COT = evaluate_responses(f'data/crows/{args.model_name}_responses_gender_cot.csv')
+
+    all_cases = [NCOT, COT]
+    case_names = ['NO_COT', 'COT']
+    all_cases_df = pd.DataFrame(all_cases)
+    all_cases_df['case'] = ['NO_COT', 'COT']
+
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M")
+    all_cases_df[['case', 'total', 'p_unknown', 'p_ster', 'p_antister']].to_csv(f'outputs/stereo/{args.model_name}_{timestamp}.csv', index=False) #inlcude date run
+    
+    # all_cases_df[['case', 'total', 'p_unknown', 'p_ster', 'p_antister']].to_csv(f'outputs/crows/{args.model_name}_{timestamp}.csv', index=False) #inlcude date run
+    
+    return
+
+if __name__=='__main__':
+    main()
